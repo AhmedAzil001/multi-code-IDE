78ba4cf17d0075929ed42b0aaeab6a396413193b
Azil Ahmed <azilahmed2016@gmail.com>
2025-01-15 23:31:08 +0530

    login page completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,43 @@
 import React from "react";
+import person from "../assets/person-fill.svg";
+import lock from "../assets/lock-fill.svg";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="absolute top-0 left-0 bottom-0 right-0 z-10 flex justify-center items-center backdrop-blur-sm bg-black/20">
-      <form action="" className="w-[25%] px-6 py-4 bg-white rounded-lg">
-        <h4 className="text-center text-2xl font-semibold mb-4">Enter your credentials</h4>
-        <div className="w-full bg-black p-2">
-          <input className="px-4 py-2 outline-none" type="email" placeholder="Email" />
+      <form
+        action=""
+        className="w-[25%] px-6 py-4 bg-white rounded-lg flex flex-col gap-2"
+      >
+        <h4 className="text-center text-2xl font-semibold mb-4">
+          Enter your credentials
+        </h4>
+        <div className="w-full py-1 px-4 flex items-center bg-neutral-100 rounded-md">
+          <img src={person} alt="" />
+          <input
+            className="px-4 py-2 outline-none bg-neutral-100"
+            type="email"
+            placeholder="Email"
+          />
         </div>
-        <div>
-          <input type="password" placeholder="Password" />
+        <div className="w-full py-1 px-4 flex items-center bg-neutral-100 rounded-md">
+          <img src={lock} alt="" />
+          <input
+            className="px-4 py-2 outline-none bg-neutral-100"
+            type="password"
+            placeholder="Password"
+          />
         </div>
-        <span>Forgot Password</span>
-        <button>Login</button>
-        <div>
-          <p>Don't have an account?</p>
-          <span>Login</span>
+        <span className="text-sm px-1 tracking-wider text-gray-800">
+          Forgot Password?
+        </span>
+        <button className="w-full px-4 py-2 my-2 flex items-center justify-center bg-black text-white rounded">
+          Login
+        </button>
+        <div className="flex items-center gap-2 px-1">
+          <p className="text-gray-800 text-sm">Don't have an account?</p>
+          <Link to={'/signup'} className="text-gray-800 text-sm font-medium">Sign Up</Link>
         </div>
       </form>
     </div>
