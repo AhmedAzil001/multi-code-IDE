3a4ef7b9297ff22c220f279d4a2e8c4fa5a64de8
Azil Ahmed <azilahmed2016@gmail.com>
2025-01-25 01:44:09 +0530

    minor changes

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB Database");
+    console.log("No error")
   } catch (error) {
     console.log(error);
   }
