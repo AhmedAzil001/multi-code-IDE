b5aa8fd5ec91a7ada52c9f1889719ea0a98a7311
Azil Ahmed <azilahmed2016@gmail.com>
2025-01-25 01:29:53 +0530

    testing

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://azilahmed2016:Azil2030Ahmed@cluster0.5ghpu.mongodb.net/code-IDE");
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
